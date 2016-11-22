d113c9133606f3cf54df3fd5f10cc84505c8160a
Pankaj-Pippin <pankaj@pippintechnologies.com>
2016-11-22 14:22:01 +0530

    php chmod

--- php/attributes/customize.rb
+++ php/attributes/customize.rb
@@ -5,3 +5,8 @@
 # "php/attributes/customize.rb" in your cookbook repository and
 # put the overrides in YOUR customize.rb file.
 ###
+node[:deploy].each do |application, deploy|
+  app_root = "#{deploy[:deploy_to]}/current"
+  execute "chmod -R 755 #{app_root}" do
+  end
+end
